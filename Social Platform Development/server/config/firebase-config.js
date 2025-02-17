099a01e0221a1875233540f988abac42789365fd
Lucas3178278745 <muyangzhou8@gmail.com>
2025-02-17 02:07:46 -0500

    Remove

--- Social Platform Development/server/config/firebase-config.js
+++
@@ -1,9 +0,0 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./serenify-82872-firebase-adminsdk-1q5lx-bc9a18629a.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
